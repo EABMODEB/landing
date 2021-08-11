1cb6353cb0bf3d6448635f979c80079c77ee0d82
BrandonArgel <brandargel@gmail.com>
2021-08-11 11:35:39 -0500

    fix conflict

--- landing/src/pages/Landing.js
+++ landing/src/pages/Landing.js
@@ -16,11 +16,8 @@ export default function Landing() {
       <section></section>
       {/* Hover images: Brandon */}
       <section></section>
-<<<<<<< HEAD
-=======
       <FormApp />
-      
->>>>>>> 246d2afec1db13f67f291c44c8ab267810941c30
+
       <section></section>
       {/* Footer: Brandon */}
       <Footer />
