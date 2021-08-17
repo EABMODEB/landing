21fa776c4bfd8d8addf9df2e392e6e93d5c63d52
BrandonArgel <brandargel@gmail.com>
2021-08-17 10:18:30 -0500

    Sucursales, Footer & Recuadro

--- landing/src/Components/Footer.js
+++ landing/src/Components/Footer.js
@@ -5,129 +5,158 @@ import InstagramIcon from "@material-ui/icons/Instagram";
 import WhatsAppIcon from "@material-ui/icons/WhatsApp";
 
 const useStyles = makeStyles((theme) => ({
-  footer: {
-    width: "100%",
-    backgroundColor: theme.palette.background.dark,
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "flex-start",
-    flexDirection: "column",
-    borderTopLeftRadius: 5,
-    borderTopRightRadius: 5,
-  },
-  footerImgContainer: {
-    width: "100%",
-    display: "flex",
-    justifyContent: "center",
-  },
-  footerImg: {
-    width: "100%",
-    maxWidth: "500px",
-    padding: "20px 20px 30px",
-    borderBottom: `2px solid ${theme.palette.background.light}`,
-  },
-  footerListContainer: {
-    width: "100%",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "space-between",
-    padding: 20,
-    boxSizing: "border-box",
-  },
-  footerList: {
-    display: "flex",
-    flexDirection: "column",
-    textAlign: "center",
-    marginBottom: 20,
-  },
-  footerListAnchor: {
-    color: theme.palette.text.light,
-    textDecoration: "none",
-    fontSize: 16,
-    "&:hover": {
-      color: "white",
-      textDecoration: "underline",
-    },
-  },
-  footerUbication: {
-    color: theme.palette.text.light,
-    fontSize: 16,
-    textAlign: "center",
-    width: 250,
-  },
-  footerIconsContainer: {
-    width: "100%",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: theme.palette.background.light,
-    padding: 20,
-  },
-  footerIconAnchor: {
-    transition: "all .3s",
-    margin: "0 15px",
+	footer: {
+		width: "100%",
+		backgroundColor: theme.palette.background.dark,
+		display: "flex",
+		justifyContent: "space-between",
+		alignItems: "flex-start",
+		flexDirection: "column",
+		borderTopLeftRadius: 5,
+		borderTopRightRadius: 5,
+	},
+	footerIconsContainer: {
+		width: "100%",
+		display: "flex",
+		justifyContent: "center",
+		alignItems: "center",
+		backgroundColor: theme.palette.background.light,
+		padding: 20,
+	},
+	footerIconAnchor: {
+		transition: "all .3s",
+		margin: "0 15px",
 
-    "&:hover": {
-      transform: "scale(1.1)",
-    },
-  },
-  footerIcon: {
-    color: "white",
-    height: 50,
-    width: 50,
-  },
+		"&:hover": {
+			transform: "scale(1.1)",
+		},
+	},
+	footerIcon: {
+		color: "white",
+		height: 50,
+		width: 50,
+	},
+	footerContainer: {
+		width: "100%",
+		maxWidth: "1200px",
+		margin: "0 auto",
+		display: "grid",
+		gridTemplateAreas: "'ubication image list'",
+		gridTemplateColumns: "1fr 50% 1fr",
+		gridTemplateRows: "1fr",
+		placeItems: "flex-end center",
+		[theme.breakpoints.down("900")]: {
+			gridTemplateAreas: "'image image''ubication list'",
+			gridTemplateColumns: "repeat(2, 1fr)",
+			gridTemplateRows: "auto",
+			placeItems: "center center",
+		},
+		[theme.breakpoints.down("500")]: {
+			gridTemplateAreas: "'image ''ubication''list'",
+			gridTemplateColumns: "1fr",
+			gridTemplateRows: "auto",
+			placeItems: "center center",
+		},
+	},
+	footerImgContainer: {
+		width: "100%",
+		maxWidth: "500px",
+		display: "flex",
+		justifyContent: "center",
+		alignItems: "center",
+		gridArea: "image",
+		padding: "20px 50px 30px",
+		borderBottom: `2px solid ${theme.palette.background.light}`,
+	},
+	footerImg: {
+		width: "100%",
+		maxWidth: "100%",
+	},
+	footerListContainer: {
+		display: "flex",
+		flexDirection: "column",
+		alignItems: "center",
+		justifyContent: "space-between",
+		padding: 20,
+		boxSizing: "border-box",
+		gridArea: "list",
+	},
+	footerList: {
+		display: "flex",
+		flexDirection: "column",
+		textAlign: "center",
+		marginBottom: 10,
+	},
+	footerListAnchor: {
+		color: theme.palette.text.light,
+		textDecoration: "none",
+		fontSize: 16,
+		"&:hover": {
+			color: "white",
+			textDecoration: "underline",
+		},
+	},
+	footerUbicationContainer: {
+		gridArea: "ubication",
+		width: "100%",
+		maxWidth: "300px",
+	},
+	footerUbication: {
+		color: theme.palette.text.light,
+		fontSize: 16,
+		textAlign: "center",
+		padding: "20px",
+		marginBottom: 10,
+	},
 }));
 
 export default function Footer() {
-  const classes = useStyles();
+	const classes = useStyles();
 
-  return (
-    <footer className={classes.footer}>
-      <div className={classes.footerIconsContainer}>
-        <a
-          className={classes.footerIconAnchor}
-          href="https://www.facebook.com/SolarCenterMX/"
-        >
-          <FacebookIcon className={classes.footerIcon} />
-        </a>
-        <a
-          className={classes.footerIconAnchor}
-          href="https://www.instagram.com/solarcentermx/"
-        >
-          <InstagramIcon className={classes.footerIcon} />
-        </a>
-        <a className={classes.footerIconAnchor} href="#">
-          <WhatsAppIcon className={classes.footerIcon} />
-        </a>
-      </div>
-      <div className={classes.footerImgContainer}>
-        <img
-          src="https://firebasestorage.googleapis.com/v0/b/landing-solar-center.appspot.com/o/assets%2FRecurso%204.png?alt=media&token=68174d8e-0fc8-4ea8-b8bf-eb0bb377f00c"
-          alt="Footer Logo"
-          className={classes.footerImg}
-        />
-      </div>
-      <ul className={classes.footerListContainer}>
-        <li className={classes.footerList}>
-          <a href="#" className={classes.footerListAnchor}>
-            Aviso de privacidad
-          </a>
-          <a href="#" className={classes.footerListAnchor}>
-            Política de garantías
-          </a>
-          <a href="#" className={classes.footerListAnchor}>
-            Política de devoluciones
-          </a>
-          <a href="#" className={classes.footerListAnchor}>
-            Política de envíos
-          </a>
-        </li>
-        <p className={classes.footerUbication}>
-          Matriz: Av. Industria Eléctrica 43-A Parque Industrial Bugambilias,
-          Tlajomulco de Zúñiga.
-        </p>
-      </ul>
-    </footer>
-  );
+	return (
+		<footer className={classes.footer}>
+			<div className={classes.footerIconsContainer}>
+				<a className={classes.footerIconAnchor} href="https://www.facebook.com/SolarCenterMX/">
+					<FacebookIcon className={classes.footerIcon} />
+				</a>
+				<a className={classes.footerIconAnchor} href="https://www.instagram.com/solarcentermx/">
+					<InstagramIcon className={classes.footerIcon} />
+				</a>
+				<a className={classes.footerIconAnchor} href="#">
+					<WhatsAppIcon className={classes.footerIcon} />
+				</a>
+			</div>
+
+			<div className={classes.footerContainer}>
+				<div className={classes.footerUbicationContainer}>
+					<p className={classes.footerUbication}>
+						Matriz: Av. Industria Eléctrica 43-A Parque Industrial Bugambilias, Tlajomulco de Zúñiga.
+					</p>
+				</div>
+				<div className={classes.footerImgContainer}>
+					<img
+						src="https://firebasestorage.googleapis.com/v0/b/landing-solar-center.appspot.com/o/assets%2FRecurso%204.png?alt=media&token=68174d8e-0fc8-4ea8-b8bf-eb0bb377f00c"
+						alt="Footer Logo"
+						className={classes.footerImg}
+					/>
+				</div>
+				<ul className={classes.footerListContainer}>
+					<li className={classes.footerList}>
+						<a href="#" className={classes.footerListAnchor}>
+							Aviso de privacidad
+						</a>
+						<a href="#" className={classes.footerListAnchor}>
+							Política de garantías
+						</a>
+						<a href="#" className={classes.footerListAnchor}>
+							Política de devoluciones
+						</a>
+						<a href="#" className={classes.footerListAnchor}>
+							Política de envíos
+						</a>
+					</li>
+				</ul>
+			</div>
+		</footer>
+	);
 }
--- landing/src/Components/sucursals.js
+++ landing/src/Components/sucursals.js
@@ -2,122 +2,122 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 
 const UseStyles = makeStyles((theme) => ({
-  sucursalContainer: {
-    textAlign: "center",
-    width: "100%",
-    maxWidth: 1240,
-    margin: "0 auto",
-  },
-  sucursalCards: {
-    width: "100%",
-    display: "flex",
-    flexDirection: "column",
-    flexWrap: "wrap",
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 20,
-    [theme.breakpoints.up("sm")]: {
-      flexDirection: "row",
-    },
-  },
-  cardContainer: {
-    width: "100%",
-    position: "relative",
-    [theme.breakpoints.up("sm")]: {
-      width: "50%",
-    },
-    [theme.breakpoints.up("md")]: {
-      width: "33%",
-    },
-    "&:hover img": {
-      filter: "brightness(0.5)",
-    },
-  },
-  cardImg: {
-    height: "100%",
-    width: "100%",
-    transition: "all .3s ease-in-out",
-  },
-  cardTextContainer: {
-    width: "100%",
-    height: "100%",
-    opacity: 0,
-    position: "absolute",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    top: 0,
-    left: 0,
-    padding: "5px",
-    transition: "opacity .3s",
-    "&:hover": {
-      opacity: 1,
-    },
-  },
-  cardText: {
-    color: theme.palette.secondary.main,
-    fontSize: "clamp(2rem, 3.3vw, 3rem)",
-  },
+	sucursalContainer: {
+		textAlign: "center",
+		width: "100%",
+		maxWidth: 1240,
+		margin: "0 auto",
+	},
+	sucursalCards: {
+		width: "100%",
+		display: "flex",
+		flexDirection: "column",
+		flexWrap: "wrap",
+		justifyContent: "center",
+		alignItems: "center",
+		padding: 20,
+		[theme.breakpoints.up("sm")]: {
+			flexDirection: "row",
+		},
+	},
+	cardContainer: {
+		width: "100%",
+		position: "relative",
+		[theme.breakpoints.up("sm")]: {
+			width: "50%",
+		},
+		[theme.breakpoints.up("md")]: {
+			width: "33%",
+		},
+		"&:hover img": {
+			filter: "brightness(0.5)",
+		},
+	},
+	cardImg: {
+		height: "100%",
+		width: "100%",
+		transition: "all .3s ease-in-out",
+	},
+	cardTextContainer: {
+		width: "100%",
+		height: "100%",
+		opacity: 0,
+		position: "absolute",
+		display: "flex",
+		justifyContent: "center",
+		alignItems: "center",
+		top: 0,
+		left: 0,
+		padding: "5px",
+		transition: "opacity .3s",
+		"&:hover": {
+			opacity: 1,
+		},
+	},
+	cardText: {
+		color: theme.palette.secondary.main,
+		fontSize: "clamp(2rem, 3.3vw, 3rem)",
+	},
 }));
 
 export default function sucursals() {
-  const classes = UseStyles();
+	const classes = UseStyles();
 
-  return (
-    <section className={classes.sucursalContainer}>
-      <h2>Sucursales y Cedis</h2>
-      <div className={classes.sucursalCards}>
-        <div className={classes.cardContainer}>
-          <img
-            className={classes.cardImg}
-            src="https://firebasestorage.googleapis.com/v0/b/landing-solar-center.appspot.com/o/imgs%2FQueretaro.jpg?alt=media&token=46e5666d-d4a4-4d25-8ba8-88814558ef8a"
-            alt="Querétaro"
-          />
-          <div className={classes.cardTextContainer}>
-            <h3 className={classes.cardText}>Querétaro</h3>
-          </div>
-        </div>
-        <div className={classes.cardContainer}>
-          <img
-            className={classes.cardImg}
-            src="https://firebasestorage.googleapis.com/v0/b/landing-solar-center.appspot.com/o/imgs%2FMonterey.jpg?alt=media&token=9dd9f805-db29-4981-bb90-67456410659b"
-            alt="Monterrey"
-          />
-          <div className={classes.cardTextContainer}>
-            <h3 className={classes.cardText}>Monterrey</h3>
-          </div>
-        </div>
-        <div className={classes.cardContainer}>
-          <img
-            className={classes.cardImg}
-            src="https://firebasestorage.googleapis.com/v0/b/landing-solar-center.appspot.com/o/imgs%2FMerida.jpg?alt=media&token=7a5e0b03-7af0-4adc-8abc-152f71cfffc8"
-            alt="Merida"
-          />
-          <div className={classes.cardTextContainer}>
-            <h3 className={classes.cardText}>Merida</h3>
-          </div>
-        </div>
-        <div className={classes.cardContainer}>
-          <img
-            className={classes.cardImg}
-            src="https://firebasestorage.googleapis.com/v0/b/landing-solar-center.appspot.com/o/imgs%2FGuadalajara.jpg?alt=media&token=a52c065c-2a9d-4a03-a574-1fa627992dc6"
-            alt="Guadalajara"
-          />
-          <div className={classes.cardTextContainer}>
-            <h3 className={classes.cardText}>Guadalajara</h3>
-          </div>
-        </div>
-        <div className={classes.cardContainer}>
-          <img
-            className={classes.cardImg}
-            src="https://firebasestorage.googleapis.com/v0/b/landing-solar-center.appspot.com/o/imgs%2FCMDX.jpg?alt=media&token=ccd3b886-5361-4ef9-a35b-fa06fe556abc"
-            alt="Ciudad de México"
-          />
-          <div className={classes.cardTextContainer}>
-            <h3 className={classes.cardText}>Ciudad de México</h3>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+	return (
+		<section className={classes.sucursalContainer}>
+			<h2>Sucursales y Cedis</h2>
+			<div className={classes.sucursalCards}>
+				<div className={classes.cardContainer}>
+					<img
+						className={classes.cardImg}
+						src="https://firebasestorage.googleapis.com/v0/b/landing-solar-center.appspot.com/o/imgs%2FGuadalajara.jpg?alt=media&token=a52c065c-2a9d-4a03-a574-1fa627992dc6"
+						alt="Guadalajara"
+					/>
+					<div className={classes.cardTextContainer}>
+						<h3 className={classes.cardText}>Guadalajara</h3>
+					</div>
+				</div>
+				<div className={classes.cardContainer}>
+					<img
+						className={classes.cardImg}
+						src="https://firebasestorage.googleapis.com/v0/b/landing-solar-center.appspot.com/o/imgs%2FMonterey.jpg?alt=media&token=9dd9f805-db29-4981-bb90-67456410659b"
+						alt="Monterrey"
+					/>
+					<div className={classes.cardTextContainer}>
+						<h3 className={classes.cardText}>Monterrey</h3>
+					</div>
+				</div>
+				<div className={classes.cardContainer}>
+					<img
+						className={classes.cardImg}
+						src="https://firebasestorage.googleapis.com/v0/b/landing-solar-center.appspot.com/o/imgs%2FQueretaro.jpg?alt=media&token=46e5666d-d4a4-4d25-8ba8-88814558ef8a"
+						alt="Querétaro"
+					/>
+					<div className={classes.cardTextContainer}>
+						<h3 className={classes.cardText}>Querétaro</h3>
+					</div>
+				</div>
+				<div className={classes.cardContainer}>
+					<img
+						className={classes.cardImg}
+						src="https://firebasestorage.googleapis.com/v0/b/landing-solar-center.appspot.com/o/imgs%2FCMDX.jpg?alt=media&token=ccd3b886-5361-4ef9-a35b-fa06fe556abc"
+						alt="Ciudad de México"
+					/>
+					<div className={classes.cardTextContainer}>
+						<h3 className={classes.cardText}>Ciudad de México</h3>
+					</div>
+				</div>
+				<div className={classes.cardContainer}>
+					<img
+						className={classes.cardImg}
+						src="https://firebasestorage.googleapis.com/v0/b/landing-solar-center.appspot.com/o/imgs%2FMerida.jpg?alt=media&token=7a5e0b03-7af0-4adc-8abc-152f71cfffc8"
+						alt="Merida"
+					/>
+					<div className={classes.cardTextContainer}>
+						<h3 className={classes.cardText}>Merida</h3>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 }
